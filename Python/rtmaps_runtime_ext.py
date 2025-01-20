1acb63839f825be4978e6549325e1918b459bda8
Lazar Lachev <LLachev@dspace.de>
2025-01-20 07:29:21 +0100

    The new source code remains under the same external license as the project code

--- Python/rtmaps_runtime_ext.py
+++ Python/rtmaps_runtime_ext.py
@@ -1,12 +1,3 @@
-# Copyright 2025 dSPACE GmbH
-
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you must not use this software except in compliance with the License. This
-# software is not fully developed or tested. It is distributed free of charge
-# and without any consideration. The software is provided "as is" in the hope
-# that it may be useful to other users, but without any warranty of any kind,
-# either express or implied. See the License for the specific language
-# governing permissions and limitations under the License.
 
 if __name__ == "__main__":  # if called from subprocess
     from rtmaps import RTMapsAbstraction
